1709e52a2003c1cbd3193623473ac96c8ad77fef
sethoak <sethoak@gmail.com>
2019-11-15 11:12:52 -0600

    saving files

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Csharp_List_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Saturn");
+            planetList.Add("Jupiter");
+        }
+        List<string> lastTwo = new List<string>() { "Pluto", "Neptune" };
+
+        lastTwo.AddRange(planetList);
+
+        Console.WriteLine("\nAddRange(planetList)");
+    }
+}
